fa11ce2efc35765a462cc402d3e62b97f117a309
전해은 <ggoo7731@naver.com>
2024-11-27 18:53:45 +0900

    ✨ Tasks.json파일을 gitignore파일에 추가
    task.json파일이 첨부되지 않도록 .gitignore에 추가

--- todo.py
+++ todo.py
@@ -1,14 +1,32 @@
-def add_task(task_name): #할 일 추가
+import json
+import os # 파이썬을 이용해서 시스템 내부에 접근이 가능하다 
+task_file = 'tasks.json'
+
+def load_task():
+    if os.path.exists('tasks.json'):#파일이 있는경우
+        with open(task_file, 'r', encoding='utf-8') as file: #file => open(task_file, 'r', encoding='utf-8') as file
+            return json.load(file) #json.load()
+    return []
+        
+def save_task(tasks): #add_task를 통해 전달받은 해야할 일을 파일에 저장하는 기능
+    with open(task_file, 'w', encoding='utf-8') as file: #file => open(TASK_FILE, 'w', encoding='utf-8')
+        json.dump(tasks, file, indent=4, ensure_ascii=False)
+
+def add_task(task_name): #할 일 추가 함수
+    tasks = load_task() # 파일이 있다면 가져와
+    task = {'name':task_name, "completed":False} #파이썬 공부하기에 대한 데이터가 들어갔어
+    tasks.append(task)
+    save_task(tasks)
+
+def view_task():#할 일 목록보기
     pass
-def view_task():
-    pass
-def complete_task(task_number): #할 일 삭제
-    pass
-def delete_task(task_number):
+def complete_task(task_number):#할 일 완료
     pass
 
+def delete_task(task_number):#할 일 삭제
+    pass
 
-def show_menu(): #기능 메뉴를 보여주는 함수
+def show_menu(): #메뉴를 보여주는 함수
     print("작업 관리 애플리케이션")
     print("1. 할 일 추가")
     print("2. 할 일 목록보기")
@@ -16,27 +34,26 @@ def show_menu(): #기능 메뉴를 보여주는 함수
     print("4. 할 일 삭제")
     print("5. 종료")
 
-def  main():
-    while True: # 무한 실행
+def main():
+    while True:
         show_menu()
-        choice = input("원하시는 기능을 선택해주세요 (1~5): ")
+        choice = input("원하는 작업을 선택해주세요 (1~5): ") # 1
 
         if choice == '1':
-            task_name = input("추가할 작업을 입력해주세요") #파이썬 공부하기
+            task_name = input("추가할 작업을 입력해주세요") # 파이썬 공부하기
             add_task(task_name)
-        elif choice == '2': #할 일 목록 보기
+        elif choice == '2':
             view_task()
-        elif choice == '3': #할 일 완료
-            task_number = int(input("완료를 원하는 작업의 번호를 입력해주세요:"))
+        elif choice == '3':
+            task_number = int(input("완료를 원하는 작업의 번호를 입력해주세요"))
             complete_task(task_number)
-
         elif choice == '4':
-            task_number = int(input("삭제를 원하는 작업의 번호를 입력해주세요:"))
+            task_number = int(input("삭제를 원하는 작업의 번호를 입력해주세요"))
             delete_task(task_number)
         elif choice == '5':
-            print('시스템을 종료합니다.')
+            print("시스템을 종료합니다.")
             break
         else:
-            print("잘못 입력하셨습니다. 1번부터 5번까지의 기능 중 하나를 선택하세요.")
+            print("잘못 입력하셨습니다. 1번부터 5번까지의 기능 중 하나를 선택해주세요")
 
 main()
