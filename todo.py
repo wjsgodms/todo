cd98ad822de7e66d81bc31f2a345ce6ef6c4cfb3
전해은 <ggoo7731@naver.com>
2024-12-03 16:40:59 +0900

    ✨ Delete_task 기능 구현

--- todo.py
+++ todo.py
@@ -16,15 +16,36 @@ def add_task(task_name): #할 일 추가 함수
     tasks = load_task() # 파일이 있다면 가져와
     task = {'name':task_name, "completed":False} #파이썬 공부하기에 대한 데이터가 들어갔어
     tasks.append(task)
-    save_task(tasks)
+    
 
-def view_task():#할 일 목록보기
-    pass
+def view_task():#할 일 목록보기, merge 진행
+    tasks = load_task() #파일이 있는 경우 안에 내용물이 타스크스에 들어가고 없으면 빈 리스크가 들어옴
+    if not tasks: #if문을 만나면 결과는 FALSE
+        print('현재 등록된 작업이 없습니다 ㅜ.ㅜ')
+    else:
+        print("작업 목록 :")
+        for i, task in enumerate(tasks, start=1): 
+            status = "완료" if task["completed"] else "미완료"
+            print(f"{i}. {task['name']} - task['completed']")
+
 def complete_task(task_number):#할 일 완료
-    pass
+    tasks = load_task()
+    if 1 <= task_number <= len(tasks):
+        tasks[task_number - 1]["completed"] = True
+        save_task(tasks)
+        print(f"할 일 : {tasks[task_number - 1]['name']} 가 완료처리 되었습니다.")
+    
+    else :
+        print("유효하지 않은 번호입니다. 다시 확인 후 입력해주세요.")
 
 def delete_task(task_number):#할 일 삭제
-    pass
+    tasks =  load_task()
+    if 1 <= task_number <= len(tasks): #. 1번부터 1번 사이
+        delete_tsk  =  tasks.pop(task_number-1)# 팝을 통해 삭제 및 반환이 되고 삭제된 데이터가 딜리트에 들어간다.
+        save_task(tasks)
+        print(f"할 일 : '{delete_tsk['name']}'이(가) 삭제되었습니다.")
+    else:
+        print("유효하지 않은 작업 번호입니다. 다시 확인해주세요")
 
 def show_menu(): #메뉴를 보여주는 함수
     print("작업 관리 애플리케이션")
