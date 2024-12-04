b2cff077ee99fb20618b09e4c55ec7f10d3345bb
전해은 <ggoo7731@naver.com>
2024-12-04 16:42:31 +0900

    ✨ 머지..
    ㅁ

--- todo.py
+++ todo.py
@@ -37,11 +37,7 @@ def complete_task(task_number):#할 일 완료
     
     else :
         print("유효하지 않은 번호입니다. 다시 확인 후 입력해주세요.")
-<<<<<<< HEAD
-=======
-
->>>>>>> divelop
-
+       
 def delete_task(task_number):#할 일 삭제
     tasks =  load_task()
     if 1 <= task_number <= len(tasks): #. 1번부터 1번 사이
